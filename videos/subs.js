08bd9d572e7fc408df1c926043cc760ded06501f
myban <76476888+myban@users.noreply.github.com>
2021-06-18 15:03:12 +0200

    Update subs.js

--- videos/subs.js
+++ videos/subs.js
@@ -1,4 +1,4 @@
-const subs = "2busty2hide+adorableporn+Babes+beautifulporn+bustyisraeligirls+cleavage+Comple+ionExcellence+FemmesFrancaises+GermansGoneWild+goddesses+GroupOfNudeGirls+happygirls+HighResNSFW+IRLgirls+LaBeauteFeminine+LatinaCuties+MiddleEasternHotties+MixedRaceGirls+naturaltitties+Nude_Selfie+oliveskin+PacificIslandGirls+palegirls+PLASTT+polishnsfw+PrettyGirls+redheads+ScottishGirlsGoneWild+SexyFrex+sexygirls+sillygirls+SoHotItHurts+spreadeagle+TurkishWomen+WomenOfColor+WomenOfColour+princesshaze__"
+const subs = "2busty2hide+adorableporn+Babes+beautifulporn+bustyisraeligirls+cleavage+Comple+ionExcellence+FemmesFrancaises+GermansGoneWild+goddesses+GroupOfNudeGirls+happygirls+HighResNSFW+IRLgirls+LaBeauteFeminine+LatinaCuties+MiddleEasternHotties+MixedRaceGirls+naturaltitties+Nude_Selfie+oliveskin+PacificIslandGirls+palegirls+PLASTT+polishnsfw+PrettyGirls+redheads+ScottishGirlsGoneWild+SexyFrex+sexygirls+sillygirls+SoHotItHurts+spreadeagle+TurkishWomen+WomenOfColor+WomenOfColour+princesshaze__";
 
 const getLink = function (domain) => {
   return "https://" + domain + ".com/" + subs;
