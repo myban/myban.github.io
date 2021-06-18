a11f8a56db93f33847340ab8169987fd874b2aa0
myban <76476888+myban@users.noreply.github.com>
2021-06-18 15:19:51 +0200

    Update subs.js

--- videos/subs.js
+++ videos/subs.js
@@ -1,5 +1,5 @@
 const subs = "2busty2hide+adorableporn+Babes+beautifulporn+bustyisraeligirls+cleavage+Comple+ionExcellence+FemmesFrancaises+GermansGoneWild+goddesses+GroupOfNudeGirls+happygirls+HighResNSFW+IRLgirls+LaBeauteFeminine+LatinaCuties+MiddleEasternHotties+MixedRaceGirls+naturaltitties+Nude_Selfie+oliveskin+PacificIslandGirls+palegirls+PLASTT+polishnsfw+PrettyGirls+redheads+ScottishGirlsGoneWild+SexyFrex+sexygirls+sillygirls+SoHotItHurts+spreadeagle+TurkishWomen+WomenOfColor+WomenOfColour+princesshaze__";
 
 function getLink(domain) {
-  return "http://" + domain + ".com/" + subs;
+  return "https://" + domain + ".com/" + subs;
 };
