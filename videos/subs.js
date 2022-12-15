ba1649bdf190362de0373b60129af3bbb1cdacda
myban <76476888+myban@users.noreply.github.com>
2022-12-15 20:21:28 +0100

    Delete videos directory

--- videos/subs.js
+++
@@ -1,5 +0,0 @@
-const subs = "2busty2hide+adorableporn+Babes+beautifulporn+bustyisraeligirls+cinnamonwomen+cleavage+ComplexionExcellence+FemmesFrancaises+GermansGoneWild+goddesses+GroupOfNudeGirls+happygirls+HighResNSFW+IRLgirls+LaBeauteFeminine+LatinaCuties+MiddleEasternHotties+MixedRaceGirls+naturaltitties+Nude_Selfie+oliveskin+PacificIslandGirls+palegirls+PLASTT+polishnsfw+PrettyGirls+redheads+ScottishGirlsGoneWild+SexyFrex+sexygirls+sillygirls+SoHotItHurts+spreadeagle+TurkishWomen+WomenOfColor+WomenOfColour+princesshaze__";
-
-function getLink(domain) {
-  return "https://" + domain + ".com/r/" + subs;
-};
